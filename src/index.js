2d3e4883904c185221675ddb1c501e06a94096b5
Deepbaran <deepbaran97@gmail.com>
2021-04-15 21:57:37 +0530

    added redux notes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,66 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+/*
+// STORE --> GLOBALIZED STATE
+
+// ACTION --> DESCRIBES WHAT YOU ARE GOING TO DO [IT DOES NOT MEAN ACTUALLY DOING THE ACTION, IT JUST DESCRIBES IT]
+const increment = () => {
+  return {
+    type: "INCEREMENT"
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
+
+// REDUCER --> DEFINES HOW THE ACTIONS TRANSFORM THE STATES INTO THE NEXT STATE [TAKES CARE OF ALL THE ACTIONS]
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCEREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+let store = createStore(counter);
+
+// Display it in console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH --> ACTION IS EXECUTED HERE
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
+
+// Reducer is going to check what action you did and based on that it is going to modify the STORE.
+// We DISPATCH the ACTION to the REDUCER and the REDUCER checks what to do and based on that, the STORE gets updated
+
+*/
+
+// const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    {/* Provider will give all the components access to the store(GLOBAL STATE) */}
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
